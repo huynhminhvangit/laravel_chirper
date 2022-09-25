58ab298b6b6c2738898d72f1fd8baac97da74c66
Vang Huynh <vanghm@imt-soft.com>
2022-09-25 22:39:59 +0700

    update chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,7 +15,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Chirps/Index', []);
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get()
+        ]);
     }
 
     /**
@@ -76,7 +78,15 @@ class ChirpController extends Controller
      */
     public function update(Request $request, Chirp $chirp)
     {
-        //
+        $this->authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
